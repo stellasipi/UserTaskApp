cb6721eee67aac4afb524b5c80e393ba7a19cd6a
Stella Tóth-Baranyi <stellasipi@gmail.com>
2022-03-11 22:00:13 +0100

    Create base spring boot project

---
+++ src/main/java/hu/tbs/usertaskapp/UserTaskAppApplication.java
@@ -0,0 +1,13 @@
+package hu.tbs.usertaskapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTaskAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserTaskAppApplication.class, args);
+	}
+
+}
