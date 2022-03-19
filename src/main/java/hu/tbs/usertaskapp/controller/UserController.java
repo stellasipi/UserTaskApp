7bcd141e216e54226909e16383b2a247f5809820
Stella Tóth-Baranyi <stellasipi@gmail.com>
2022-03-19 17:55:45 +0100

    Add all endpoints without implementation

--- src/main/java/hu/tbs/usertaskapp/controller/UserController.java
+++ src/main/java/hu/tbs/usertaskapp/controller/UserController.java
@@ -1,15 +1,67 @@
 package hu.tbs.usertaskapp.controller;
 
+import hu.tbs.usertaskapp.model.dto.TaskDTO;
+import hu.tbs.usertaskapp.model.dto.UserDTO;
+import hu.tbs.usertaskapp.service.TaskService;
 import hu.tbs.usertaskapp.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 @AllArgsConstructor
+@Slf4j
 public class UserController {
 
     private UserService userService;
+
+    private TaskService taskService;
+
+    @GetMapping(consumes = "application/json", produces = "application/json")
+    public ResponseEntity<UserDTO> getAllUsers() {
+        return null;
+    }
+
+    @GetMapping(value = "/{id}", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<UserDTO> getUser() {
+        return null;
+    }
+
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public ResponseEntity<UserDTO> createUser() {
+        return null;
+    }
+
+    @PutMapping(value = "/{id}", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<UserDTO> updateUser() {
+        return null;
+    }
+
+    @GetMapping(value = "/{user_id}/task", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<TaskDTO> getAllTasksForUser() {
+        return null;
+    }
+
+    @GetMapping(value = "/{user_id}/task/{task_id}", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<TaskDTO> getTaskForUser() {
+        return null;
+    }
+
+    @PostMapping(value = "/{user_id}/task", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<TaskDTO> createTask() {
+        return null;
+    }
+
+    @PutMapping(value = "/{user_id}/task/{task_id}", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<TaskDTO> updateTask() {
+        return null;
+    }
+
+    @DeleteMapping(value = "/{user_id}/task/{task_id}", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<TaskDTO> deleteTask() {
+        return null;
+    }
 
 }
