cba7f3fda941a1b862b0c8bddcd0575a9521ac1e
Stella Tóth-Baranyi <stellasipi@gmail.com>
2022-03-25 00:58:45 +0100

    Add database migration

--- src/main/java/hu/tbs/usertaskapp/UserTaskAppApplication.java
+++ src/main/java/hu/tbs/usertaskapp/UserTaskAppApplication.java
@@ -1,8 +1,12 @@
 package hu.tbs.usertaskapp;
 
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableScheduling
@@ -10,6 +14,11 @@ public class UserTaskAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserTaskAppApplication.class, args);
+    }
+
+    @Autowired
+    public UserTaskAppApplication(DataSource dataSource) {
+        Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
     }
 
 }
